ae725582a60ec2f389e824afa96313723b6f27d5
pressstart101 <walterscott404@yandex.com>
2016-07-06 13:32:38 -0400

    🐼

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -117,7 +117,6 @@ average_f(1, b: 3, c: 5) == 3.0
 average_i(1, b: 3, c: 5) > 1 && average_i(1, b: 3, c: 5) < 5
 
 
-
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
  */
